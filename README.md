68f2b3b875a97d019f014c44e20d630361e8f278
Meg Meyers <megan.g.meyers@gmail.com>
2020-01-17 22:28:34 -0800

    post done in routing and html. modal showing friend working

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 working on using more elements of bootstrap rather than using my own css file,
 form validtion
 fa icons
+pushing myself to use more efficientjs 
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -9,13 +9,36 @@ app.get("/api/friends", (req, res) => {
 
 
 app.post("/api/friends", (req, res) => {
+    
+    var matchedFriend;
 
-    //recieve newFriend object via ajax post
-    //survey compatability logic 
-    //send most compatbile friend object as response
+    //get the user's score total
+    var scoreTotal = req.body.scores.reduce((a,  b) => parseInt(a) + parseInt(b), 0);
+    console.log(`scoreTotal: ${scoreTotal}`);
 
+    //the max difference between scores is 50-10 = 40
+    var diff = 40;
 
+    //get score totals for each friend in the database and save if abs diff is less
+    friendsData.forEach(friend => {
+        var tempSum = friend.scores.reduce((a,b) => parseInt(a) + parseInt(b), 0)
+        if (Math.abs(tempSum-scoreTotal) < diff){
+            diff = Math.abs(tempSum-scoreTotal);
+            matchedFriend = friend;      
+        }
+    });
+    
+    console.log(matchedFriend)
+
+    //push last so we don't compare to ourself during above loop
+    friendsData.push(req.body)
+
+    //send the matched friend 
+    res.json(matchedFriend);
 });
 
 
 }
+
+
+
