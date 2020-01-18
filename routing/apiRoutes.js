c574f5cf9d4f5387095cb8cbe5c0f30cd970a71c
Meg Meyers <megan.g.meyers@gmail.com>
2020-01-17 18:39:43 -0800

    survey submit btn done

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,10 +5,17 @@ module.exports = function(app) {
 
 app.get("/api/friends", (req, res) => {
     res.json(friendsData)
-})
+});
 
 
+app.post("/api/friends", (req, res) => {
 
+    //recieve newFriend object via ajax post
+    //survey compatability logic 
+    //send most compatbile friend object as response
+
+
+});
 
 
 }
